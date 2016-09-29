194c81a5f90081396537fc56281814f70d7f295c
Gregor Best <gbe@unobtanium.de>
2016-09-29 13:44:15 +0200

    Add a default configuration

---
+++ config.def.py
@@ -0,0 +1,11 @@
+aps = { b"ThisIsMySSID": "ThisIsMyPassword", b"ThisIsAnOpenSSID": "" }
+
+mqtt = {
+    "host": "my.mqtt.host",
+    "id": "my.id",
+    "auth": {
+        "user": "this-is-my-user",
+        "password": "and-this-is-the-password",
+        "ssl": True
+    }
+}
